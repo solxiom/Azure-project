6b83e6904729bb048eb8d4907d85bbfaa9edf700
Kavan Soleimanbeigi <solxiom@gmail.com>
2013-03-07 20:25:19 +0200

    folder structure changes..

---
+++ src/azure/control/MainController.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package azure.control;
+
+/**
+ *
+ * @author kavansol
+ */
+public class MainController {
+    
+}
