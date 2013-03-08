eb6431ef6992248a5dab87a62653e886aab6dc03
kavan soleimanbeigi <solxiom@gmail.com>
2013-03-08 14:35:26 +0200

    basic view components added ....

--- src/azure/control/MainController.java
+++ src/azure/control/MainController.java
@@ -1,9 +1,12 @@
 package azure.control;
 
 import azure.domain.SampleObject;
+import javax.servlet.ServletContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 //import org.springframework.ui.Model;
 //import org.springframework.web.bind.annotation.ModelAttribute;
@@ -16,11 +19,27 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MainController {
     
+    @Autowired
+    private ServletContext servletContext;
+    
+    
     @RequestMapping(value = "/")
     public String index() {
 
         System.out.println("Hello from photomash index");
         return "index";
+    }
+    @RequestMapping(value = "/album/create")
+    public String createAlbum() {
+
+        
+        return "create_album";
+    }
+    @RequestMapping(value = "/album/create/submit", method = RequestMethod.POST)
+    public String submitNewAlbum() {
+
+        System.out.println("I got the infos!!");
+        return "create_album";
     }
     @RequestMapping(value = "/photo/{img_id}")
     public @ResponseBody SampleObject getShopInJSON(@PathVariable String img_id) {
